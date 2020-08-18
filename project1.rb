3e150d4da6a040ae8f701f66dedf7e16610ba401
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-08-18 20:32:23 +0300

    Fix linting errors

--- project1.rb
+++ project1.rb
@@ -1,46 +1,41 @@
 # -------------------Bubble sort----------------
 
-
 def bubble_sort(array)
   sweeps_needed = 1
-  while  sweeps_needed > 0 do
+  while sweeps_needed.positive?
     sweeps_needed = 0
-    for i in 0..array.length-2
-      if array[i] > array[i+1]
-          z = array[i]
-          array[i] = array[i+1]
-          array[i+1] = z
-          sweeps_needed = 1
+    (0..array.length - 2).each do |i|
+      if array[i] > array[i + 1]
+        z = array[i]
+        array[i] = array[i + 1]
+        array[ i+1 ] = z
+        sweeps_needed = 1
       end
     end
   end
   array
 end
 
-array = [6,4,2,1,3,14,8,-1]
+array = [6, 4, 2, 1, 3, 14, 8, -1]
 
 p bubble_sort(array)
-
 
 # ---------------Bubble-sort-by---------------------
 
 def bubble_sort_by(array)
   sweeps_needed = 1
-  while  sweeps_needed > 0 do
+  while sweeps_needed.positive?
     sweeps_needed = 0
-    for i in 0..array.length-2
-      if yield(array[i], array[i+1]) > 0
+    (0..array.length - 2).each do |i|
+      if yield(array[i], array[i + 1]).positive?
         z = array[i]
-          array[i] = array[i+1]
-          array[i+1] = z
-          sweeps_needed = 1
+        array[i] = array[i + 1]
+        array[i + 1] = z
+        sweeps_needed = 1
       end
     end
   end
   array
 end
 
-p (bubble_sort_by(["hi","hello","hey"]) do |left, right|
-  left.length - right.length
-  end)
-  
+p ( bubble_sort_by(%w[hi hello hey]) {|left, right| left.length - right.length })
