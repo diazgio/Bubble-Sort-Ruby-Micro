caeb83e8cb66d68d7a485af800c10736d4b13fe9
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-08-18 18:06:16 +0300

    Add while loop

--- project1.rb
+++ project1.rb
@@ -1,16 +1,21 @@
 # Bubble sort
 
-array = [9,6,4,5,8,2,3,1]
+array = [9,6,4,5,8,2,3,1,0,12,]
 
 def bubble_sort(array)
-    i=0
-    for i in 0..array.length
-        if array[i] > array[i+1]
-            z = array[i]
-            array[i] = array[i+1]
-            array[i+1] = z
-        end
+  sweeps_needed = 1
+  while  sweeps_needed > 0 do
+    sweeps_needed = 0
+    for i in 0..array.length-2
+      if array[i] > array[i+1]
+          z = array[i]
+          array[i] = array[i+1]
+          array[i+1] = z
+          sweeps_needed = 1
+      end
     end
-    puts array
+  end
+  puts array
 end
+
 bubble_sort(array)
